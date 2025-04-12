cf17c78faaf90799f31bb15e8475498a87f0c071
Tim Pilius <tpilius@gmail.com>
2025-04-12 19:42:00 -0400

    test

--- BuildAndPackage.ps1
+++ BuildAndPackage.ps1
@@ -1,10 +1,15 @@
 # TODO clean repo first
 Push-Location $PSScriptRoot
 
+Remove-Item ./dist -Force -Recurse
 mkdir ./dist -ErrorAction SilentlyContinue
 
 # Building BepinEx
 dotnet run --project BepinEx/build/Build.csproj -- --target MakeDist
 Copy-Item -Recurse BepinEx/bin/dist/Unity.Mono-win-x64/. ./dist/Bepinex
+
+# Copying libraries and scripts
+Copy-Item -Recurse libraries ./dist/Libraries
+Copy-Item -Recurse Scripts/* ./dist
 
 Pop-Location
