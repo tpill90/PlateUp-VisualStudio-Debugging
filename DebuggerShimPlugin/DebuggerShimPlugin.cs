c516db133fbe75d730d0fa091a6e484b2c6d1a93
Tim Pilius <tpilius@gmail.com>
2025-04-14 12:34:01 -0400

    Now generating mdb on plugin launch

--- DebuggerShimPlugin/DebuggerShimPlugin.cs
+++ DebuggerShimPlugin/DebuggerShimPlugin.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 
 namespace DebuggerShimPlugin
 {
@@ -32,17 +33,38 @@ namespace DebuggerShimPlugin
             log.LogInfo($"Found {filesInDir.Count} mod .dlls");
             foreach (var file in filesInDir)
             {
-                GenerateMdb(file, pdb2mdbPath);
+                var fileNameNoExtension = Path.GetFileNameWithoutExtension(file.FullName);
+                var pdbPath = $"{file.Directory}\\{fileNameNoExtension}.pdb";
+
+                // Can't generate an .mdb if there isn't a .pdb
+                if (!File.Exists(pdbPath))
+                {
+                    log.LogError($"pdb not found for {file.Name}.  Will not be able to debug in Visual Studio.  " +
+                                  $"The .pdb must be generated as a 'Full' .pdb and copied into the mod directory.");
+                }
+
+                if (File.Exists(pdbPath))
+                {
+                    GenerateMdb(file, pdbPath, pdb2mdbPath);
+                }
 
                 Assembly assembly = Assembly.LoadFrom(file.FullName);
-                log.LogInfo($"      Loaded {assembly.FullName}");
-
-
+                log.LogInfo($"Loaded {assembly.FullName}");
             }
         }
 
-        private void GenerateMdb(FileInfo file, string pdbToMdbPath)
+        //TODO refactor this signature + general refactor + comment
+        private static void GenerateMdb(FileInfo file, string pdbPath, string pdbToMdbPath)
         {
+            var fileNameNoExtension = Path.GetFileNameWithoutExtension(file.FullName);
+            FileInfo pdbFileInfo = new FileInfo($"{file.Directory}\\{fileNameNoExtension}.pdb");
+            FileInfo mdbFileInfo = new FileInfo($"{file.Directory}\\{fileNameNoExtension}.dll.mdb");
+
+            if (mdbFileInfo.Exists && pdbFileInfo.LastWriteTime < mdbFileInfo.LastWriteTime)
+            {
+                return;
+            }
+
             var process = new Process
             {
                 StartInfo =
@@ -57,6 +79,8 @@ namespace DebuggerShimPlugin
 
             process.Start();
             process.WaitForExit();
+
+            log.LogWarning($"Generated new .mdb for {file.Name}");
         }
     }
 }
